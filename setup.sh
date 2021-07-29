aaaa5ba3cc3a4f70137f688de666e7bd48b3fb40
Ayaan-20 <73228148+Ayaan-20@users.noreply.github.com>
2021-07-29 17:50:14 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,4 +8,9 @@ echo "\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+[theme]
+primaryColor='#85878a'
+backgroundColor='#000000'
+secondaryBackgroundColor='#212123'
+textColor='#1d2020'
 " > ~/.streamlit/config.toml
